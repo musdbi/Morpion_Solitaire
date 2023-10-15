1ce90cf1d90bd91e8ab1912f2aa6b7d68a8d80a2
Pi-erren <pierr@LAPTOP-03OQ4QKS>
2023-10-15 18:45:17 +0200

    class with diagram (next)

--- Morpion_Solitaire/src/components/Grid.java
+++ Morpion_Solitaire/src/components/Grid.java
@@ -1,14 +1,28 @@
 package components;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Grid {
 	
 	private List<Point> gridPoints;
 	
 	private List<Point> playablePoints;
 	
-	private int size
+	private int size;
+	
+	private List<Line> lines;
+	
+	private Point maxPlayablePoint;
+	
+	private Point minPlayablePoint;
+
 	public Grid() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void addPlayedPoint() {
+		
+	}
+	
 }
