9f7bd758d7414b0e8a65f1a67a9e54fce23c3b1a
Pi-erren <pierr@LAPTOP-03OQ4QKS>
2023-10-18 19:20:27 +0200

    merge feature/implement_diagram

---
+++ Morpion_Solitaire/src/tests/PointTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PointTest {
+
+	public PointTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
