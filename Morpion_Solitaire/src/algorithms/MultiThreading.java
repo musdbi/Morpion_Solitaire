a759a6608cb14603632980bf87b39bbfdc9e3258
Pi-erren <pneveu937@gmail.com>
2023-11-17 19:34:45 +0100

    better implementation for multiThread

--- Morpion_Solitaire/src/algorithms/MultiThreading.java
+++ Morpion_Solitaire/src/algorithms/MultiThreading.java
@@ -1,5 +1,10 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class MultiThreading extends Thread{
 	
 	/**
@@ -17,10 +22,21 @@ public class MultiThreading extends Thread{
 	 */
 	private int iterations;
 	
+	
+	/**
+	 * The number of times one thread will launch the algorithm
+	 * 
+	 * = math.ceil(this.iterations / numberOfThreads)
+	 * 
+	 * We round up the result 
+	 */
+	private int threadIterations;
+		
 	public MultiThreading(ResearchAlgorithm algo, int numberOfThreads, int iterations) {
 		this.algo = algo;
 		this.numberOfThreads = numberOfThreads;
 		this.iterations = iterations;
+		this.threadIterations = (int) Math.ceil(this.iterations / numberOfThreads);
 	}
 	
 	@Override
@@ -30,29 +46,31 @@ public class MultiThreading extends Thread{
 	
 	public void trainAlgorithm() throws InterruptedException {
 		long startTime = System.currentTimeMillis();
-		MultiThreading thread1 = new MultiThreading(algo, numberOfThreads, iterations);
-		MultiThreading thread2 = new MultiThreading(algo, numberOfThreads, iterations);
-		MultiThreading thread3 = new MultiThreading(algo, numberOfThreads, iterations);
+		
+		List<MultiThreading> threads = new ArrayList<>(); // List to keep order of thread for .join()
+		for(int i = 0; i < this.numberOfThreads; i++) {
+			MultiThreading thread = new MultiThreading(algo, numberOfThreads, threadIterations);
+			threads.add(thread);
+			thread.start();
+		}
+		
+		for(MultiThreading thread: threads) {
+			thread.join();
+		}
 
-		thread1.start();
-		thread2.start();
-		thread3.start();
-		
-		thread1.join();
-		thread2.join();
-		thread3.join();
-		
 		long endTime = System.currentTimeMillis();
         double elapsedTime = (endTime - startTime) * 0.001;
         System.out.println("Time taken: " + elapsedTime + " seconds");
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
-		MultiThreading randomMultiThread = new MultiThreading(new RandomAlgorithm(), 3, 34);
+		MultiThreading randomMultiThread = new MultiThreading(new RandomAlgorithm(), 4, 100);
 		randomMultiThread.trainAlgorithm();
-		System.out.println("Nombres d'itérations au total: " + ResearchAlgorithm.getScores().size());
 		ResearchAlgorithm.calculateStatistics();
-		System.out.println("Sur 100 coups:");
+		System.out.println("Nombres d'itérations au total: " + ResearchAlgorithm.getScores().size());
+		System.out.println("Nombres d'itérations : par thread " + randomMultiThread.threadIterations);
+		System.out.println("Sur "+ randomMultiThread.iterations + " coups:");
+		System.out.println("Number of threads: " + randomMultiThread.numberOfThreads);;
 		System.out.println("Le score est en moyenne: " + ResearchAlgorithm.getMean());
 		System.out.println("L'écart-type est: " + ResearchAlgorithm.getSigma());
 	}
