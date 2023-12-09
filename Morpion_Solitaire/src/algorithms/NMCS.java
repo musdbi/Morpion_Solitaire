b03d579b831ff6f623dc129715a4e52cc6d68507
Pi-erren <pneveu937@gmail.com>
2023-12-09 16:51:29 +0100

    tests for differents levels of NMCS

--- Morpion_Solitaire/src/algorithms/NMCS.java
+++ Morpion_Solitaire/src/algorithms/NMCS.java
@@ -23,11 +23,13 @@ public class NMCS implements ResearchAlgorithm{
 	@Override
 	public int algorithm() {
 		NMCSState initialState = setUpInitState();
-		int score = NMCS(initialState, depthLevel);
+		long debut = System.currentTimeMillis();
+        long dureeMax = 1000000 * 1000; // 30 secondes en millisecondes
+		int score = NMCS(initialState, depthLevel, debut, dureeMax);
 		return score;
 	}
 
-	public int NMCS(NMCSState state, int level) {
+	public int NMCS(NMCSState state, int level, long debut, long dureeMax) {
 		if (level == 0) {
 			return state.scoreFromRollout();
 		}
@@ -36,7 +38,10 @@ public class NMCS implements ResearchAlgorithm{
 		int bestScore = -1;
 //		while (!state.isTerminal()) {
 			for (NMCSState childState: state.getChilds()) {
-				int currentChildScore = NMCS(childState, level -1);
+				if (System.currentTimeMillis() - debut > dureeMax) {
+	                break; // Sortir de la boucle si la durée est dépassée
+	            }
+				int currentChildScore = NMCS(childState, level -1, debut, dureeMax);
 				if (currentChildScore > bestScore ) bestScore = currentChildScore;
 			}
 //		}
@@ -53,20 +58,34 @@ public class NMCS implements ResearchAlgorithm{
 		NMCS nmcs0 = new NMCS(0);
 		NMCS nmcs1 = new NMCS(1);
 		NMCS nmcs2 = new NMCS(2);
-		
-		System.out.println("Score pour une recherch de profondeur 0: " + nmcs0.algorithm());	
-		long endTime = System.currentTimeMillis();
+
+		NMCS nmcs3 = new NMCS(3);
+		NMCS nmcs4 = new NMCS(4);
+
+//		
+//		System.out.println("Score pour une recherch de profondeur 0: " + nmcs0.algorithm());	
+//		long endTime = System.currentTimeMillis();
+//		double elapsedTime = (endTime - startTime) * 0.001;
+//        System.out.println("Time taken: " + elapsedTime + " seconds");
+//        
+//		System.out.println("Score pour une recherch de profondeur 1: " + nmcs1.algorithm());
+//		endTime = System.currentTimeMillis();
+//		elapsedTime = (endTime - startTime) * 0.001;
+//        System.out.println("Time taken: " + elapsedTime + " seconds");
+        
+        System.out.println("Score pour une recherch de profondeur 2: " + nmcs2.algorithm());
+		double endTime = System.currentTimeMillis();
 		double elapsedTime = (endTime - startTime) * 0.001;
         System.out.println("Time taken: " + elapsedTime + " seconds");
         
-		System.out.println("Score pour une recherch de profondeur 1: " + nmcs1.algorithm());
-		endTime = System.currentTimeMillis();
-		elapsedTime = (endTime - startTime) * 0.001;
-        System.out.println("Time taken: " + elapsedTime + " seconds");
-        
-        System.out.println("Score pour une recherch de profondeur 2: " + nmcs2.algorithm());
-		endTime = System.currentTimeMillis();
-		elapsedTime = (endTime - startTime) * 0.001;
-        System.out.println("Time taken: " + elapsedTime + " seconds");
+//        System.out.println("Score pour une recherch de profondeur 3: " + nmcs3.algorithm());
+//		endTime = System.currentTimeMillis();
+//		elapsedTime = (endTime - startTime) * 0.001;
+//        System.out.println("Time taken: " + elapsedTime + " seconds");
+//        
+//        System.out.println("Score pour une recherch de profondeur 4: " + nmcs4.algorithm());
+//		endTime = System.currentTimeMillis();
+//		elapsedTime = (endTime - startTime) * 0.001;
+//        System.out.println("Time taken: " + elapsedTime + " seconds");
 	}
 }
