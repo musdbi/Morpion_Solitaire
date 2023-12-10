84b6d3b55595990c9109a3a1a641b4e4ad69ef0c
Pi-erren <pneveu937@gmail.com>
2023-12-10 13:34:06 +0100

    NMCS multi threading

---
+++ Morpion_Solitaire/src/algorithms/NMCSWorker.java
@@ -0,0 +1,27 @@
+package algorithms;
+
+import java.util.concurrent.Callable;
+
+import components.Grid;
+
+public class NMCSWorker implements Callable<Grid>{
+    private final NMCS nmcs;
+	private final NMCSState childState;
+    private final int level;
+    private final long debut;
+    private final long dureeMax;
+
+    public NMCSWorker(NMCS nmcs,NMCSState childState, int level, long debut, long dureeMax) {
+        this.nmcs = nmcs;
+        this.childState = childState;
+        this.level = level;
+        this.debut = debut;
+        this.dureeMax = dureeMax;
+    }
+
+	@Override
+    public Grid call() {
+        return nmcs.nmcs(childState, level, debut, dureeMax);
+    }
+
+}
