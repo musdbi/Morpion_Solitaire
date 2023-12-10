092f9e1a6e5c105b442dc79473cc77fe50443091
musdbi <mustapha.doubabi@gmail.com>
2023-12-10 17:23:35 +0100

    Translate the game into english

--- Morpion_Solitaire/src/application/MSSceneController.java
+++ Morpion_Solitaire/src/application/MSSceneController.java
@@ -46,9 +46,9 @@ public class MSSceneController {
 	public void switchToGame (ActionEvent event) throws IOException {
 	    if (nomJoueur.getText().isEmpty()) {
 	    	Alert alert = new Alert(Alert.AlertType.ERROR);
-	        alert.setTitle("Erreur");
-	        alert.setHeaderText("Nom manquant");
-	        alert.setContentText("Veuillez entrer un nom !");
+	        alert.setTitle("Error");
+	        alert.setHeaderText("Missing Name");
+	        alert.setContentText("Please enter a name !");
 	        alert.showAndWait();
 	    } else {
 	    	
