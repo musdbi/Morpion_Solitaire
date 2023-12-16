b56be02393c6d7a4d0e9ab0ec896c55354bd0fe0
musdbi <mustapha.doubabi@gmail.com>
2023-12-16 16:38:19 +0100

    FINAL PUSH

--- Morpion_Solitaire/src/application/GraphicController.java
+++ Morpion_Solitaire/src/application/GraphicController.java
@@ -16,27 +16,22 @@ public class GraphicController {
 	
 	@FXML
 	private Stage stage;
-	
-	private static Map<String, Double> score0;
-	private static Map<String, Double> score1;
-	private static Map<String, Double> score2;
-	private static Map<String, Double> score3;
 	@FXML
 	private AreaChart<Number, Number> chart;
 	
 	public void initialize () {
-		if (score0 != null) {
-			plotGaussianCurve(score0.get("Mean"), score0.get("Variance"));
+		if (ResearchAlgorithmController.getScore0() != null) {
+			plotGaussianCurve(ResearchAlgorithmController.getScore0().get("Mean"), ResearchAlgorithmController.getScore0().get("Variance"));
 		}
-		if (score1 != null) {
-		    plotGaussianCurve(score1.get("Mean"), score1.get("Variance"));
+		if (ResearchAlgorithmController.getScore1() != null) {
+		    plotGaussianCurve(ResearchAlgorithmController.getScore1().get("Mean"), ResearchAlgorithmController.getScore1().get("Variance"));
 		}
-		if (score2 != null) {
-		    plotGaussianCurve(score2.get("Mean"), score2.get("Variance"));
+		if (ResearchAlgorithmController.getScore2() != null) {
+		    plotGaussianCurve(ResearchAlgorithmController.getScore2().get("Mean"), ResearchAlgorithmController.getScore2().get("Variance"));
 		}
-//		if (score3 != null) {
-//		    plotGaussianCurve(score3.get("Mean"), score3.get("Variance"));
-//		}
+		if (ResearchAlgorithmController.getScore3() != null) {
+		    plotGaussianCurve(ResearchAlgorithmController.getScore3().get("Mean"), ResearchAlgorithmController.getScore3().get("Variance"));
+		}
 	}
 	
 	public void plotGaussianCurve(double mean, double variance) {
@@ -44,22 +39,6 @@ public class GraphicController {
         XYChart.Series<Number, Number> series = gaussianCurve.createGaussianSeries();
         chart.getData().add(series);
     }
-	
-	public static void setScore0(Map<String, Double> score) {
-		score0 = score;
-	}
-	
-	public static void setScore1(Map<String, Double> score) {
-		score1 = score;
-	}
-	
-	public static void setScore2(Map<String, Double> score) {
-		score2 = score;
-	}
-	
-	public static void setScore3(Map<String, Double> score) {
-		score3 = score;
-	}
 	
 	public void switchToMenu (ActionEvent event) {
 		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
