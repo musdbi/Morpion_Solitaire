4169db778597cb2a4439bca7e2aba22e76b5a07e
musdbi <mustapha.doubabi@gmail.com>
2023-12-18 11:06:34 +0100

    Adding unit tests for point/played point/line

--- Morpion_Solitaire/src/test/PointTest.java
+++ Morpion_Solitaire/src/test/PointTest.java
@@ -3,10 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Objects;
-
+import components.Grid;
 import components.PlayedPoint;
 import components.Point;
 import helpers.Direction;
+import helpers.exceptions.OutOfGridException;
 
 public class PointTest {
 	private Point p1 = new Point(1,1);
@@ -53,4 +54,45 @@ public class PointTest {
 				playedPoint.getInvolvedDirections().size() == 1
 				);
 	}
+	
+	@Test
+    public void testConstructorAndGetter() {
+        assertEquals(1, p4.getX());
+        assertEquals(4, p4.getY());
+    }
+	
+	@Test
+    public void testConstructorNegativeCoordinates() {
+        assertThrows(OutOfGridException.class, () -> new Point(-1, 1));
+        assertThrows(OutOfGridException.class, () -> new Point(1, -1));
+    }
+	
+	@Test
+    public void testConstructorOutOfGrid() {
+        assertThrows(OutOfGridException.class, () -> new Point(Grid.getSize(), 1));
+        assertThrows(OutOfGridException.class, () -> new Point(1, Grid.getSize()));
+    }
+	
+	@Test
+    public void testToString() {
+        assertEquals("(1,5)", p5.toString());
+    }
+	
+	@Test
+    public void testPlayedPointConstructor() {
+        PlayedPoint playedPoint = new PlayedPoint(4, 5);
+        assertEquals(4, playedPoint.getX());
+        assertEquals(5, playedPoint.getY());
+        assertFalse(playedPoint.isEndOfLine());
+        playedPoint.setEndOfLine(true);
+        assertTrue(playedPoint.isEndOfLine());
+    }
+	
+	@Test
+    public void testHashCodeConsistencyWithPlayedPoint() {
+        Point point = new Point(2, 3);
+        PlayedPoint playedPoint = new PlayedPoint(2, 3);
+        assertEquals(point.hashCode(), playedPoint.hashCode(), "Point and PlayedPoint with same coordinates should have the same hashCode");
+    }
+	
 }
